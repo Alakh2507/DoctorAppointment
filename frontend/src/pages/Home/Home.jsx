0ba99428c1a992226d83f0217e2f5d791649fc52
alakh niranjan <alakh.kushwaha1@gmil.com>
2026-04-08 00:55:07 +0530

    update Topdoctor

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header.jsx'
 import SpecialityMenu from '../../components/SpecialityMenu/SpecialityMenu.jsx'
-import TopDoctors from '../../components/topDoctors/topDoctors.jsx'
+import TopDoctors from '../../components/TopDoctors/TopDoctors.jsx'
 import BookAppointment from '../../components/BookAppointment/BookAppointment.jsx'
 
 
-const Home=()=>{
+const Home = () => {
     return (
         <div className='homecontainer'>
-        <Header/>
-        <SpecialityMenu/>
-        <TopDoctors/>
-        <BookAppointment/>
+            <Header />
+            <SpecialityMenu />
+            <TopDoctors />
+            <BookAppointment />
         </div>
     )
 }
